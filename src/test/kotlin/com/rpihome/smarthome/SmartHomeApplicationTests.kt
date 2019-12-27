c4a1f7a1cab57d8ea5265abe97d9d6fb3c1f1b50
davicl cho! <4332247+david-kwchoi@users.noreply.github.com>
2019-12-27 18:06:22 +0800

    initial commit for smart home base skeleton from spring boot

---
+++ src/test/kotlin/com/rpihome/smarthome/SmartHomeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rpihome.smarthome
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmartHomeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
